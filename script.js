0688cb2c37ddd32bd34f4332ba93df3bbb704fa9
rdsq <62446963+rdsq@users.noreply.github.com>
2024-06-12 18:54:24 +0300

    PWA share?

---
+++ script.js
@@ -0,0 +1,33 @@
+// register PWA service worker for shared text receiving
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./pwa-share-target.js').then(
+        (registration) => {
+            console.log("Service worker registration succeeded:", registration);
+        },
+        (error) => {
+            console.error(`Service worker registration failed: ${error}`);
+        },
+    )
+} else {
+    console.error("Service workers are not supported.");
+}
+
+const editorElement = document.getElementById('editor');
+
+function hashChangeHandler() {
+    // if there were some arguments
+    if (location.hash !== '' && location.hash !== '#') {
+        const text = decodeURIComponent(location.hash.substring(1));
+        // clear arguments
+        location.hash = "";
+        // set text
+        editorElement.innerText = text;
+        console.log(text, editorElement)
+    }
+}
+
+window.addEventListener('hashchange', hashChangeHandler, false);
+// I don't know why `false`, this is how it was on MDN
+
+// also call it initially
+hashChangeHandler();
